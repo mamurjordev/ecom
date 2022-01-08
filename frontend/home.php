d4f39fc0104edc53ab3ae84119daf50591853a0a
azonedev <abdullah914115@gmail.com>
2022-01-08 23:19:57 +0600

    frontend template mastering

---
+++ frontend/home.php
@@ -0,0 +1,14 @@
+<?php require_once "partials/header.php" ?>
+    
+<main>
+
+<?php require_once "home/slider.php" ?>
+<?php require_once "home/new_product.php" ?>
+<?php require_once "home/gallery.php" ?>
+<?php require_once "home/popular.php" ?>
+<?php require_once "home/info.php" ?>
+
+
+</main>
+
+<?php require_once 'partials/footer.php'; ?>
