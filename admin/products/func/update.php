ad1f43f4a979d4b60a05e84da01f908b9ca1af79
azonedev <abdullah914115@gmail.com>
2022-01-29 21:47:15 +0600

    Feature image update & unlink previous one

--- admin/products/func/update.php
+++ admin/products/func/update.php
@@ -12,12 +12,36 @@ require_once __DIR__.'/../../../config.php';
         $description = $_POST['description'];
 
         $id = $_REQUEST['id'];
+        // temp-userid variable until make auth
+        $user_id = rand(1,3);
 
-        $sql = "UPDATE products SET category_id='$category_id', user_id='$user_id', product_name='$product_name',description = '$description', quantity = '$quantity', price='$price', discount_price='$discount_price' WHERE product_id='$id'";
+        // esisting feature image
+        $sql = "SELECT feature_image FROM products WHERE product_id='$id'";
+        $data = $connection->query($sql);
+        foreach($data as $value){
+            $existing_feature_image = $value['feature_image'];
+        }
 
+        // feature image
+        if(isset($_FILES['feature_image'])){
+            $image = $_FILES['feature_image'];
+
+            // feature image
+            $temp_location =  $image['tmp_name'];
+            $image_name = 'img/'.time().'.webp';
+            move_uploaded_file($temp_location,$image_name);
+
+            // delete existing feature image
+            unlink($existing_feature_image);
+        }else{
+            $image_name = $existing_feature_image;
+        }
+
+        $sql = "UPDATE products SET category_id='$category_id', user_id='$user_id', product_name='$product_name',description = '$description', quantity = '$quantity', price='$price', discount_price='$discount_price', feature_image='$image_name' WHERE product_id='$id'";
 
 
         $result = $connection->query($sql);
+
 
         if($result){
             echo "Product inserted successfully";
