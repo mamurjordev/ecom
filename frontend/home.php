6479bf248719d89e84c6fe5356c71e2feb7b5fc9
azonedev <abdullah914115@gmail.com>
2022-02-08 21:34:27 +0600

    Latest product

--- frontend/home.php
+++ frontend/home.php
@@ -5,7 +5,7 @@
 
 <?php require_once "home/slider.php" ?>
 <?php require_once "home/top_discount.php" ?>
-<?php require_once "home/gallery.php" ?>
+<?php require_once "home/latest_product.php" ?>
 <?php require_once "home/popular.php" ?>
 <?php require_once "home/info.php" ?>
 
