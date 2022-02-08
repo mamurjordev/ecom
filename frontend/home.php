6499a60700f4a4062a6e552d36554b54d52a327c
azonedev <abdullah914115@gmail.com>
2022-02-08 21:39:50 +0600

    Sold out section

--- frontend/home.php
+++ frontend/home.php
@@ -6,6 +6,7 @@
 <?php require_once "home/slider.php" ?>
 <?php require_once "home/top_discount.php" ?>
 <?php require_once "home/latest_product.php" ?>
+<?php require_once "home/sold_out.php" ?>
 <?php require_once "home/popular.php" ?>
 <?php require_once "home/info.php" ?>
 
