86742e5ada5fa44488082b3fdd54071ffecf870a
azonedev <abdullah914115@gmail.com>
2022-02-19 21:33:37 +0600

    Update cart process logic

--- cart_process.php
+++ cart_process.php
@@ -7,8 +7,21 @@ if(isset($_REQUEST['product_id'])){
     // cart info
     $product_id = $_REQUEST['product_id'];
     $user_id = $_SESSION['user_id'];
+    $qty = 1;
 
-    $sql = "INSERT INTO carts(user_id,product_id) VALUES( '$user_id', '$product_id')";
+    // get product
+    $getProductSql = "SELECT * from products where product_id='$product_id'";
+    $productQuery = $connection->query($getProductSql);
+    foreach ($productQuery as $productData) //retrive data form productQuery
+
+    // check discount have or not
+    if($productData['discount_price']!=null){
+        $product_price = $productData['price'];
+    }else{
+        $product_price = $productData['discount_price'];
+    }
+
+    $sql = "INSERT INTO carts(user_id,product_id,buy_price, qty) VALUES( '$user_id', '$product_id', '$product_price','$qty')";
 
     $query = $connection->query($sql);
     if($query){
